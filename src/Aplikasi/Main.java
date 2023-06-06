8b25ec280e5aa5f5bfd529c27dde6bc3c6ed04e5
adamjun99 <adam.jundi101@gmail.com>
2023-06-06 14:20:46 +0700

    tambah file java

---
+++ src/Aplikasi/Main.java
@@ -0,0 +1,28 @@
+package Aplikasi;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    
+        FXMLLoader loader = new FXMLLoader((getClass().getResource("loginPage.fxml")));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("hungerhero");
+        primaryStage.show();
+        primaryStage.setFullScreen(true);
+    }
+}
