621e61fd0856ae43ecabc64c9752eea4282fcd92
Vladimir <22523298@students.uii.ac.id>
2023-06-07 09:57:21 +0700

    iya

---
+++ src/Aplikasi/User.java
@@ -0,0 +1,29 @@
+package Aplikasi;
+
+public class User {
+
+    private String name;
+    private String email;
+    private String password;
+    private String phoneNumber;
+
+    public User(String name,String email, String password, String phoneMumber){
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
