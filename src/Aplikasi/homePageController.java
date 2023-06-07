ce090fce4e9c96037c210b62d7c97885bf232ace
adamjun99 <adam.jundi101@gmail.com>
2023-06-07 22:58:14 +0700

    login & sign up

---
+++ src/Aplikasi/homePageController.java
@@ -0,0 +1,16 @@
+package Aplikasi;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class homePageController implements Initializable{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+    }
+    
+}
--- src/Aplikasi/loginPageController.java
+++ src/Aplikasi/loginPageController.java
@@ -1,72 +1,42 @@
 package Aplikasi;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.animation.*;
-import javafx.application.*;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
-import javafx.util.Duration;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
-public class loginPageController implements Initializable {
+public class loginPageController implements Initializable{
 
     @FXML
-    private VBox vbox1;
+    private TextField tfEmail;
+
     @FXML
-    private ImageView imageView1;
+    private PasswordField pfPassword;
+
     @FXML
-    private ImageView imageView2;
-    private int currentImageIndex = 0;
+    private Hyperlink hlSignup;
+
+    @FXML
+    private Hyperlink hlForgotPassword;
+
+    @FXML
+    private void login(ActionEvent event){
+    }
+
+    @FXML
+    private void signup (ActionEvent event) throws IOException{
+    };
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        imageView1 = new ImageView();
-        imageView2 = new ImageView();
-
-        imageView1.setImage(new Image(getClass().getResourceAsStream("login.jpg")));
-        imageView2.setImage(new Image(getClass().getResourceAsStream("elaine-casap-qgHGDbbSNm8-unsplash.jpg")));
-        vbox1.getChildren().addAll(imageView1, imageView2);
-
-        // Start the slideshow
-        startSlideshow();
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
     }
-
-    public void startSlideshow() {
-        Duration duration = Duration.seconds(3);
-
-        // Create a Timeline for the slideshow animation
-        Timeline timeline = new Timeline(
-                new KeyFrame(duration, event -> showNextImage()));
-
-        // Set the animation to repeat indefinitely
-        timeline.setCycleCount(Timeline.INDEFINITE);
-
-        // Start the animation
-        timeline.play();
-    }
-
-    private void showNextImage() {
-        // Increment the current image index
-        currentImageIndex++;
-
-        // Determine the index of the next image to be displayed
-        int nextImageIndex = currentImageIndex % 2;
-
-        // Load the next image and update the appropriate ImageView
-        if (nextImageIndex == 0) {
-            imageView1.setImage(new Image(getClass().getResourceAsStream("image1.jpg")));
-        } else {
-            imageView2.setImage(new Image(getClass().getResourceAsStream("image2.jpg")));
-        }
-    }
+    
 }
