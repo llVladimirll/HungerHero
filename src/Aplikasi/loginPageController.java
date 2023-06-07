a266033a1a359bda4af92ce67b3cb4ef84f37aeb
Vladimir <22523298@students.uii.ac.id>
2023-06-07 10:56:27 +0700

    iya

--- src/Aplikasi/loginPageController.java
+++ src/Aplikasi/loginPageController.java
@@ -1,6 +1,5 @@
 package Aplikasi;
 
-
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -28,55 +27,51 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
-
-
 public class loginPageController implements Initializable {
 
-@FXML
-private TextField tfEmail;
+    @FXML
+    private TextField tfEmail;
 
-@FXML 
-private PasswordField pfPassword;
+    @FXML
+    private PasswordField pfPassword;
 
-@FXML
-private void login(){
-String Email = tfEmail.getText();
-String Password = pfPassword.getText();
+    @FXML
+    private void login() {
+        String Email = tfEmail.getText();
+        String Password = pfPassword.getText();
+        if (isValidLogin(Email, Password)) {
 
-}
+        } else {
 
-private boolean isValidLogin(String email, String password){
-    try{
-        File xmlFile = new File("Users.xml");
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder db = dbFactory.newDocumentBuilder();
-        Document doc = db.parse(xmlFile);
-        doc.getDocumentElement().normalize();
-
-        NodeList nodeList = doc.getElementsByTagName("user");
-        for (int i = 0; i < nodeList.getLength(); i++){
-            Node node = nodeList.item(i);
-            if(node.getNodeType() == Node.ELEMENT_NODE){
-                Element element = (Element) node;
-                String storedEmail = element.getElementsByTagName("email").item(0).getTextContent();
-                String storedPassword = element.getElementsByTagName("password").item(0).getTextContent();
-                if(storedEmail.equals(email) && storedPassword.equals(password)){
-                    return true;
-                }
-            }
         }
-        
-            }catch(Exception e){
-                e.printStackTrace();
-            }
-            return false;
-        }
-
 
     }
 
+    private boolean isValidLogin(String email, String password) {
+        try {
+            File xmlFile = new File("Users.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbFactory.newDocumentBuilder();
+            Document doc = db.parse(xmlFile);
+            doc.getDocumentElement().normalize();
 
-   
+            NodeList nodeList = doc.getElementsByTagName("user");
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+                    String storedEmail = element.getElementsByTagName("email").item(0).getTextContent();
+                    String storedPassword = element.getElementsByTagName("password").item(0).getTextContent();
+                    if (storedEmail.equals(email) && storedPassword.equals(password)) {
+                        return true;
+                    }
+                }
+            }
 
-   
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
+}
