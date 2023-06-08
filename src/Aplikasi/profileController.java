d71531c6b55fe33dfe2a14609df6ada5ae73f688
adamjun99 <adam.jundi101@gmail.com>
2023-06-08 12:51:40 +0700

    add profile and home pag

---
+++ src/Aplikasi/profileController.java
@@ -0,0 +1,18 @@
+package Aplikasi;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class profileController implements Initializable{
+
+    
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+    }
+    
+}
